deff1d140ca54c8cf98076c5aed4ee0f31c2e9f5
Anastahasia <anasthasia.assani@gmail.com>
2023-07-05 11:57:40 +0400

    mise en forme de la section 1 et début de la section 3

--- composants/section3.php
+++ composants/section3.php
@@ -5,18 +5,20 @@
             <p class="intro">What makes us unique</p>
             <h2>Why to choose our salon </h2>
 
-            <div>
+            <div id="listes">
                 <ul>
-                    <li>Lorem Ipsum Dolor</li>
-                    <li>Lorem Ipsum Dolor</li>
-                    <li>Lorem Ipsum Dolor</li>
-                    <li>Lorem Ipsum Dolor</li>
-                    <li>Lorem Ipsum Dolor</li>
-                    <li>Lorem Ipsum Dolor</li>
+                    <li><i class="fa-solid fa-circle-check" style="color: #eea191; font-size: 300%"></i>Lorem Ipsum Dolor</li>
+                    <li><i class="fa-solid fa-circle-check" style="color: #eea191; font-size: 300%"></i>Lorem Ipsum Dolor</li>
+                    <li><i class="fa-solid fa-circle-check" style="color: #eea191; font-size: 300%"></i>Lorem Ipsum Dolor</li>
+                </ul>    
+                <ul>
+                    <li><i class="fa-solid fa-circle-check" style="color: #eea191; font-size: 300%"></i>Lorem Ipsum Dolor</li>
+                    <li><i class="fa-solid fa-circle-check" style="color: #eea191; font-size: 300%"></i>Lorem Ipsum Dolor</li>
+                    <li><i class="fa-solid fa-circle-check" style="color: #eea191; font-size: 300%"></i>Lorem Ipsum Dolor</li>
                 </ul>
             </div>
         </div>
-        <div>
+        <div id="femme">
             <img src="images/whytochoose.png" alt="jeune femme">
         </div>
     </div>
