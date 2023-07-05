77b7ad998eb7b605af1b200047bc8a46add6a36b
Anastahasia <anasthasia.assani@gmail.com>
2023-07-05 10:23:42 +0400

    capture écran accueil

--- composants/section1.php
+++ composants/section1.php
@@ -2,18 +2,19 @@
     
     <img src="images/logo2.png" alt="">
     <div class="titre">
-        <h1><em>Welcome </em>
-in beauty paradise
+        <h1>
+            <em>Welcome </em>
+            <br>in beauty paradise
         </h1>
     </div>
-    <p>In publishing and graphic design,
-lorem ipsum is common to cry</p>
+    <p id="soustitre">In publishing and graphic design,
+        <br>lorem ipsum is common to cry</p>
     <div class="bouton">
         <a href="">Book a massage</a>
     </div>
 
     <div id="fleche">
-        <p >&#129123;</p>
+        <a href="#partTwo"><p>&#129123;</p></a>
     </div>
     
 </section>
